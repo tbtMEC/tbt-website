211ec38baf8b45296b836d45930210dcbfe0b8ec
Medha K <medhakvinod@gmail.com>
2022-09-25 17:18:13 +0530

    changes to reader

--- src/sections/ReadersForum.jsx
+++ src/sections/ReadersForum.jsx
@@ -3,24 +3,36 @@ import "../styles/readersForum.scss";
 const ReadersForum = () => {
   return (
     <div className="section" id="readersForum">
-      Readers Forum
       <div className="flexbox-item flexbox-1">
-        <img src="/assets/smallbook.png" alt="book " width={"35px"} />
-        <h2>The Readers Forum</h2>
-      </div>
-      <div>
         <img
-          className=" flexbox-item flexbox-2"
+          className="book"
+          src="/assets/smallbook.png"
+          alt="book"
+          height="66.19px"
+          width="100px"
+        />
+        <h1 className="head">The Readers Forum</h1>
+      </div>
+      <div className="flexbox-item">
+        <img
+          className=" child"
           src="/assets/child-sitting-on-books.png"
           alt="reading"
         />
       </div>
       <div className="flexbox-item flexbox-3">
         <p>
-          The readers forum is a place where we celebrate the art of reading, be
-          it books, essays, or posters. In this Booked Corner, meet other
-          readers and let your wings of imagination fly.
+          The reader’s forum is a place where we celebrate the art of reading,
+          be it books, essays, catalogues, or posters. In this Booked Corner,
+          meet other readers and let your wings of imagination fly.
         </p>
+        <img
+          className="book1"
+          src="/assets/smallbook.png"
+          alt="book"
+          height="80vh"
+          width="80vh"
+        />
       </div>
     </div>
   );
