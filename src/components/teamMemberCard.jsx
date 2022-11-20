e30e04e70f2508ce3c250dd3b0c0210d2b1792c3
Aldrin Jenson <aldrinjenson@gmail.com>
2022-11-21 01:05:56 +0530

    show Core team 2021 in layout

--- src/components/teamMemberCard.jsx
+++ src/components/teamMemberCard.jsx
@@ -1,6 +1,26 @@
 const TeamMemberCard = ({ member }) => {
-  console.log(member);
-  return <div>TeamMemberCard</div>;
+  const { name, designation, imagePath } = member;
+
+  return (
+    <div>
+      <div
+        className="column"
+        style={{
+          marginBottom: 30,
+        }}
+      >
+        <div className="border">
+          <img
+            src={`/teamMembers21Images/${imagePath}`}
+            alt={designation}
+            style={{ width: "100%", height: "100%" }}
+          />
+          <p style={{ fontWeight: "bold" }}>{name}</p>
+          <p>{designation}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TeamMemberCard;
