edbfa7cd9678c6f5eacfd10d32b90e15ee086fab
Aldrin Jenson <aldrinjenson@gmail.com>
2022-11-30 11:20:42 +0530

    add music on load

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import VisionPage from "./pages/Vision";
 import ErrorPage from "./pages/error-page";
+import ReactAudioPlayer from "react-audio-player";
+import MagicInTheAir from "./magicInTheAir.mp3";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
@@ -24,6 +27,7 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
+    <ReactAudioPlayer src={MagicInTheAir} autoPlay={true} loop />
     <RouterProvider router={router}>{/* <App /> */}</RouterProvider>
   </React.StrictMode>
 );
