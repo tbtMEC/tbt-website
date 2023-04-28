6b272b35c93f4ee895f67991026b086cf549b7ab
Ashik K L <ashikkl19112001@gmail.com>
2023-04-28 22:13:40 +0530

    new navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -20,7 +20,9 @@ export default function Navbar({ sidebarActive, setSidebarActive }) {
       window.removeEventListener("resize", changeWidth);
     };
   }, []);
-
+  if (sidebarActive && screenWidth > 992) {
+    setSidebarActive(false);
+  }
   return (
     <nav>
       {!sidebarActive ? (
@@ -32,52 +34,88 @@ export default function Navbar({ sidebarActive, setSidebarActive }) {
       ) : null}
 
       {(sidebarActive || screenWidth > 992) && (
-        <ul className="list">
-          <li className="items">
-            <HashLink onClick={() => setSidebarActive(false)} to="/#about">
-              About
-            </HashLink>
-          </li>
-          <li className="items">
-            <HashLink
-              onClick={() => setSidebarActive(false)}
-              to="/#readersForum"
-            >
-              The Reader's Forum
-            </HashLink>
-          </li>
-          <li className="items">
-            <HashLink
-              onClick={() => setSidebarActive(false)}
-              to="/#writersForum"
-            >
-              The Writer's Forum
-            </HashLink>
-          </li>
-          <li className="items">
-            <HashLink
-              onClick={() => setSidebarActive(false)}
-              to="/#speakersForum"
-            >
-              The Speaker's Forum
-            </HashLink>
-          </li>
-          <li className="items">
-            <HashLink onClick={() => setSidebarActive(false)} to="/#events">
-              Events
-            </HashLink>
-          </li>
-          <li className="items">
-            <HashLink onClick={() => setSidebarActive(false)} to="/#team">
-              Team
-            </HashLink>
-          </li>
-          <li className="items">
+        <div className="list">
+          <div className="dropdown">
+            <button className="dropbtn" href="/">
+              <HashLink onClick={() => setSidebarActive(false)} to="/">
+                Home
+              </HashLink>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-caret-down-fill"
+                viewBox="0 0 16 16"
+              >
+                {" "}
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />{" "}
+              </svg>
+            </button>
+            <div className="dropdown-content">
+              <div className="items">
+                <HashLink onClick={() => setSidebarActive(false)} to="/#about">
+                  About
+                </HashLink>
+              </div>
+              <div className="items">
+                <HashLink
+                  onClick={() => setSidebarActive(false)}
+                  to="/#readersForum"
+                >
+                  The Reader's Forum
+                </HashLink>
+              </div>
+              <div className="items">
+                <HashLink
+                  onClick={() => setSidebarActive(false)}
+                  to="/#speakersForum"
+                >
+                  The Speaker's Forum
+                </HashLink>
+              </div>
+              <div className="items">
+                <HashLink
+                  onClick={() => setSidebarActive(false)}
+                  to="/#writersForum"
+                >
+                  The Writer's Forum
+                </HashLink>
+              </div>
+              <div className="items">
+                <HashLink onClick={() => setSidebarActive(false)} to="/#events">
+                  Major Initiatives
+                </HashLink>
+              </div>
+              <div className="items">
+                <HashLink onClick={() => setSidebarActive(false)} to="/#team">
+                  Core Team
+                </HashLink>
+              </div>
+            </div>
+          </div>
+          <div className="items">
             <HashLink onClick={() => setSidebarActive(false)} to="/vision">
               Vision
             </HashLink>
-          </li>
-        </ul>
+          </div>
+          <div className="items">
+            <a
+              onClick={() => setSidebarActive(false)}
+              href="https://event-bridge-alpha.vercel.app/"
+            >
+              Events
+            </a>
+          </div>
+          <div className="items">
+            <a
+              onClick={() => setSidebarActive(false)}
+              href="https://tbt-book-exchange.netlify.app/"
+            >
+              Book Exchange
+            </a>
+          </div>
+        </div>
       )}
 
       <button onClick={toggleNav} className="btnmenu">
