e6d34173e0fb02a02a264ae95ff6125e1bfe562d
Ashik K L <ashikkl19112001@gmail.com>
2023-05-16 18:21:30 +0530

    change: old team styling

--- src/sections/NewTeamList.jsx
+++ src/sections/NewTeamList.jsx
@@ -1,36 +1,50 @@
 import React from "react";
-import TeamMemberCard from "../components/teamMemberCard";
 import teamMembers21 from "../teamMembers21.json";
 import "../styles/newTeam.css";
 
 const TeamSection = () => {
-  const [
-    firstMember,
-    secondMember,
-    thirdMember,
-    fourthMember,
-    ...otherMembers
-  ] = teamMembers21;
+  const data=teamMembers21
+  function makeCard(data) {
+    return (
+      <div key={data.designation} className="column">
+        <div className="border">
+          <div className="avatarBG">
+            <div className="avatar">
+              <img src={"teamMembers21Images/" + data.image} alt="image" />
+            </div>
+          </div>
+          <p>
+            {data.name}
+            <br></br>
+            {data.designation}
+          </p>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="section" id="newTeam">
-      <div className="head">Core Team 2021</div>
-
-      <div className="flex wrap center">
-        <TeamMemberCard member={firstMember} />
+    <div className="section" id="team">
+      <div className="head">Our Team</div>
+      <div className="row">
+        {data.slice(0, 1).map((item) => {
+          return makeCard(item);
+        })}
       </div>
-
-      <div className="flexCenterRow">
-        <TeamMemberCard member={secondMember} />
-        <TeamMemberCard member={thirdMember} />
-        <TeamMemberCard member={fourthMember} />
-        <br />
+      <div className="row">
+        {data.slice(1, 4).map((item) => {
+          return makeCard(item);
+        })}
       </div>
-
-      <div className="flexCenterRow">
-        {otherMembers.map((member, index) => (
-          <TeamMemberCard key={index} member={member} />
-        ))}
+      <div className="row">
+        {data.slice(4, 7).map((item) => {
+          return makeCard(item);
+        })}
       </div>
+      <div className="row">
+        {data.slice(7, 11).map((item) => {
+          return makeCard(item);
+        })}
+      </div>{" "}
     </div>
   );
 };
